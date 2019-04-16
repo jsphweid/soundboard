370720696a280a8e0a973ceeb9b1e13a6abe2e1a
Joseph Weidinger <josephweidinger@carfax.com>
2019-04-15 20:18:59 -0500

    goof

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 import Board from '../components/board'
 
 import './index.css'
-import './react-contextmenu.css'
 import LoadBoardModal from '../components/load-board-modal'
 import SaveBoardModal from '../components/save-board-modal'
 import { getStores } from '../stores'
